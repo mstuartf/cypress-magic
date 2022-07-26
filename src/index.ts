8854e41b65404df2d31062eb5ab05f6321437e11
mikef <mstuartflood@gmail.com>
2022-07-26 17:58:43 +0100

    remove async code

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,38 @@ import {finder} from '@medv/finder';
 
 const EVENTS: ParsedEvent[] = [];
 
-function parseEvent(event: Event): Promise<ParsedEvent> {
-  return new Promise((resolve, reject) => {
-    let selector: string;
-    if ((event.target as Element).hasAttribute('data-cy')) {
-      selector = `[data-cy=${(event.target as Element).getAttribute('data-cy')}]`;
-    } else if ((event.target as Element).hasAttribute('data-test')) {
-      selector = `[data-test=${(event.target as Element).getAttribute('data-test')}]`;
-    } else if ((event.target as Element).hasAttribute('data-testid')) {
-      selector = `[data-testid=${(event.target as Element).getAttribute('data-testid')}]`;
-    } else {
-      selector = finder(event.target as Element, { attr: (name, value) => name === 'data-cy' || name === 'data-test' || name === 'data-testid' });
-    }
-    const parsedEvent: ParsedEvent = {
-      selector,
-      action: event.type,
-      tag: (event.target as Element).tagName,
-      value: (event.target as HTMLInputElement).value,
-    };
-    if ((event.target as HTMLAnchorElement).hasAttribute('href')) parsedEvent.href = (event.target as HTMLAnchorElement).href;
-    if ((event.target as Element).hasAttribute('id')) parsedEvent.id = (event.target as Element).id;
-    if (parsedEvent.tag === 'INPUT') parsedEvent.inputType = (event.target as HTMLInputElement).type;
-    if (event.type === 'keydown') parsedEvent.key = (event as KeyboardEvent).key;
-    resolve(parsedEvent);
-  });
+function parseEvent(event: Event): ParsedEvent {
+  let selector: string;
+  if ((event.target as Element).hasAttribute('data-cy')) {
+    selector = `[data-cy=${(event.target as Element).getAttribute('data-cy')}]`;
+  } else if ((event.target as Element).hasAttribute('data-test')) {
+    selector = `[data-test=${(event.target as Element).getAttribute('data-test')}]`;
+  } else if ((event.target as Element).hasAttribute('data-testid')) {
+    selector = `[data-testid=${(event.target as Element).getAttribute('data-testid')}]`;
+  } else {
+    selector = finder(event.target as Element, { attr: (name, value) => name === 'data-cy' || name === 'data-test' || name === 'data-testid' });
+  }
+  const parsedEvent: ParsedEvent = {
+    selector,
+    action: event.type,
+    tag: (event.target as Element).tagName,
+    value: (event.target as HTMLInputElement).value,
+  };
+  if ((event.target as HTMLAnchorElement).hasAttribute('href')) parsedEvent.href = (event.target as HTMLAnchorElement).href;
+  if ((event.target as Element).hasAttribute('id')) parsedEvent.id = (event.target as Element).id;
+  if (parsedEvent.tag === 'INPUT') parsedEvent.inputType = (event.target as HTMLInputElement).type;
+  if (event.type === 'keydown') parsedEvent.key = (event as KeyboardEvent).key;
+  return parsedEvent;
 }
 
 /**
  * Checks if DOM event was triggered by user; if so, it calls parseEvent on the data.
  * @param event
  */
-async function handleEvent(event: Event): Promise<void> {
+function handleEvent(event: Event): void {
   if (event.isTrusted === true) {
     try {
-      EVENTS.push(await parseEvent(event));
+      EVENTS.push(parseEvent(event));
     } catch (error) {
       throw new Error(error as any);
     }
