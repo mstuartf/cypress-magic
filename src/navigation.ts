5abe394d08cfdc11c214e4dfe2783a91ee85e202
mikef <mstuartflood@gmail.com>
2022-07-28 14:26:57 +0100

    patch all history methods

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,21 +1,41 @@
 function monkeyPatchHistory (history: History, register: (event: any) => void) {
     const pushState = history.pushState;
     history.pushState = function(state, unused, url) {
-        register({
-            type: 'navigate',
-            url,
-            title: '?'
-        })
+        register({type: 'navigate', url})
         return pushState.apply(history, arguments);
     }
+
     const replaceState = history.replaceState;
     history.replaceState = function(state, unused, url) {
+        register({type: 'navigate', url})
+        return replaceState.apply(history, arguments);
+    }
+
+    const back = history.back;
+    history.back = function() {
         register({
             type: 'navigate',
-            url,
-            title: '?'
+            url: 'back',
         })
-        return replaceState.apply(history, arguments);
+        return back.apply(history, arguments);
+    }
+
+    const forward = history.forward;
+    history.forward = function() {
+        register({
+            type: 'navigate',
+            url: 'forward',
+        })
+        return forward.apply(history, arguments);
+    }
+
+    const go = history.go;
+    history.go = function(delta) {
+        register({
+            type: 'navigate',
+            delta,
+        })
+        return go.apply(history, arguments);
     }
 }
 
