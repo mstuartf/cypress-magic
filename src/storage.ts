3476c7552dccb8c3ae49235cf2c1c87f32a17194
mikef <mstuartflood@gmail.com>
2022-09-15 12:07:41 +0100

    remove duplicate storage events

--- src/storage.ts
+++ src/storage.ts
@@ -2,27 +2,48 @@ import { SaveEvent, StorageEvent } from "./types";
 import storageChanged from "storage-changed";
 import { obfuscateObj } from "./obfuscate";
 
-const localStorageSnapshotEvent: () => StorageEvent = () => ({
+const getSnapshot = () =>
+  Object.entries(localStorage)
+    .filter(([, v]) => typeof v !== "function")
+    .reduce((prev, [k, v]) => {
+      let val: any;
+      try {
+        val = JSON.parse(v);
+      } catch (e) {
+        val = v;
+      }
+      return {
+        ...prev,
+        [k]: val,
+      };
+    }, {});
+
+const buildEvent: (snapshot: any) => StorageEvent = (snapshot) => ({
   type: "storage",
   timestamp: Date.now(),
   domain: window.location.hostname,
-  value: obfuscateObj(
-    Object.entries(localStorage)
-      .filter(([, v]) => typeof v !== "function")
-      .reduce(
-        (prev, [k, v]) => ({
-          ...prev,
-          [k]: v,
-        }),
-        {}
-      )
-  ),
+  value: obfuscateObj(snapshot),
 });
 
+const snapshotManager = (saveEventFn: SaveEvent) => {
+  let prev: string = "";
+
+  const save = () => {
+    const snapshot = getSnapshot();
+    const snapShotString = JSON.stringify(snapshot);
+    if (snapShotString === prev) {
+      return;
+    }
+    saveEventFn(buildEvent(snapshot));
+    prev = snapShotString;
+  };
+
+  return { save };
+};
+
 export const initializeStorage = (saveEvent: SaveEvent) => {
+  const { save } = snapshotManager(saveEvent);
   storageChanged("local");
-  window.addEventListener("storageChanged", () =>
-    saveEvent(localStorageSnapshotEvent())
-  );
-  saveEvent(localStorageSnapshotEvent());
+  window.addEventListener("localStorageChanged", save);
+  save();
 };
