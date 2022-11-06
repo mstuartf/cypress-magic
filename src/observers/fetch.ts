82dad15eac418f85a908707151a20da7ae7c8481
mikef <mstuartflood@gmail.com>
2022-11-06 17:15:32 +0000

    manually patch fetch api

--- src/observers/fetch.ts
+++ src/observers/fetch.ts
@@ -1,98 +1,86 @@
-// Listens for API calls and responses
+import { InitArgs, ObfuscateFn, RequestEvent, ResponseEvent } from "../types";
 
-import { register } from "fetch-intercept";
-import {
-  InitArgs,
-  PerformanceResourceEvent,
-  RequestEvent,
-  ResponseEvent,
-} from "../types";
-import { createErrorEvent } from "../utils/createErrorEvent";
+const isRequestObj = (input: RequestInfo | URL): input is Request => {
+  return (input as Request).method !== undefined;
+};
 
-const parseRequest: (url: string, init?: RequestInit) => RequestEvent = (
-  url,
-  init
-) => ({
-  type: "request",
-  timestamp: Date.now(),
-  url,
-  method: init ? init.method : "GET",
-});
+const isURL = (input: string | URL): input is URL => {
+  return (input as URL).hostname !== undefined;
+};
 
-const parseResponse: (
-  res: Response & { request: Request }
-) => Promise<ResponseEvent> = (response) => {
+const parseRequest = (
+  input: RequestInfo | URL,
+  init?: RequestInit
+): RequestEvent => {
+  let url: string;
+  let method: string;
+
+  if (isRequestObj(input)) {
+    url = input.url;
+    method = input.method;
+  } else if (isURL(input)) {
+    url = input.toString();
+    method = init ? init.method : "GET";
+  } else {
+    url = input;
+    method = init ? init.method : "GET";
+  }
+
+  return {
+    type: "request",
+    timestamp: Date.now(),
+    url,
+    method,
+  };
+};
+
+const parseResponse = (
+  response: Response,
+  method: string,
+  obfuscate: ObfuscateFn
+): Promise<ResponseEvent> => {
   return new Promise((resolve, reject) => {
-    const {
-      request: { url, method },
-      status,
-    } = response;
+    const { url, status } = response;
     const shared: Omit<ResponseEvent, "body"> = {
       type: "response",
       timestamp: Date.now(),
-      url,
       method,
+      url,
       status,
     };
     response
       .clone()
       .json()
-      .then((body) => resolve({ ...shared, body }))
+      .then((body) => resolve({ ...shared, body: obfuscate(body) }))
       .catch(() => resolve({ ...shared, body: null }));
   });
 };
 
-const getInitialPerformanceData = (): PerformanceResourceEvent => {
-  const entries = window.performance.getEntriesByType(
-    "resource"
-  ) as PerformanceResourceTiming[];
-  return {
-    type: "performance",
-    timestamp: Date.now(),
-    resources: entries.map(({ name, initiatorType }) => ({
-      name,
-      initiatorType,
-    })),
-  };
-};
-
-export const initFetchObserver = ({
+export function initFetchObserver({
   saveEvent,
   obfuscate,
   registerOnCloseCallback,
-}: InitArgs) => {
-  let first = true;
-  const unregister = register({
-    request: function (url, config) {
-      try {
-        saveEvent(parseRequest(url, config));
-        if (first) {
-          // This is the first request we have been able to intercept but not necessarily the first request made by the app.
-          // Send a history of requests already made so we can detect this on the backend.
-          saveEvent(getInitialPerformanceData());
-          first = false;
-        }
-      } catch (e) {
-        saveEvent(createErrorEvent("request", e));
-      }
-      return [url, config];
-    },
+}: InitArgs) {
+  const { fetch: originalFetch } = window;
+  window.fetch = async (...args) => {
+    const requestEvent = parseRequest(...args);
+    saveEvent(requestEvent);
 
-    response: function (response) {
-      try {
-        parseResponse(response)
-          .then(({ body, ...rest }) => {
-            saveEvent({
-              ...rest,
-              body: body ? obfuscate(body) : body,
-            });
-          })
-          .catch((e) => saveEvent(createErrorEvent("response", e)));
-      } catch (e) {
-        saveEvent(createErrorEvent("response", e));
-      }
-      return response;
-    },
-  });
-  registerOnCloseCallback(unregister);
-};
+    const response = await originalFetch(...args);
+
+    const responseEvent = await parseResponse(
+      response,
+      requestEvent.method,
+      obfuscate
+    );
+    saveEvent(responseEvent);
+
+    return response;
+  };
+
+  const removePatch = () => {
+    window.fetch = originalFetch;
+  };
+
+  registerOnCloseCallback(removePatch);
+}
