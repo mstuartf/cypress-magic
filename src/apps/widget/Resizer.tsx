5d67c36051a62394e18f5fca6c20ea387dd03c97
Mike Flood <mike.flood@complyadvantage.com>
2024-03-14 16:54:49 +0000

    styling

--- src/apps/widget/Resizer.tsx
+++ src/apps/widget/Resizer.tsx
@@ -42,7 +42,7 @@ const Resizer = ({ children }: { children: React.ReactNode }) => {
   }, [innerWidth]);
   return (
     <div
-      className="fixed top-0 right-0 bottom-0 border-2 border-red-500 bg-white"
+      className="fixed top-0 right-0 bottom-0 border-l-2 border-gray-500 bg-slate-50"
       style={{ width: `${sideBarWith}px` }}
     >
       {children}
