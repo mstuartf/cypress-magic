f0ef9616e221eae9f73c58e721fa7a32393acbb4
Mike Flood <mike.flood@complyadvantage.com>
2024-03-14 15:11:55 +0000

    move resizing to separate file

--- src/apps/widget/App.tsx
+++ src/apps/widget/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectEvents } from "./redux/selectors";
 import { ParsedEvent, UserEvent } from "../../plugin/types";
@@ -6,62 +6,17 @@ import { removeEvent, saveEvent } from "./redux/slice";
 import initialize from "../../plugin/initialize";
 import { toast, ToastContainer, ToastTransition } from "react-toastify";
 import { CloseButtonProps } from "react-toastify/dist/components";
-import { widgetId } from "../../widget";
-import { useWindowSize } from "./hooks/useWindowSize";
 import { useNewToast } from "./hooks/useNewToast";
+import { sideBarWith } from "./constants";
+import Resizer from "./Resizer";
 
 const getEventId = (event: ParsedEvent) => `${event.type}-${event.timestamp}`;
-
-const sideBarWith = 362;
-
-const getFixedElements = (): HTMLElement[] =>
-  Array.prototype.slice
-    .call(document.body.getElementsByTagName("*"))
-    .filter(
-      (elem) =>
-        window.getComputedStyle(elem, null).getPropertyValue("position") ==
-        "fixed"
-    )
-    .filter((elem) => !document.getElementById(widgetId)!.contains(elem));
-
-const getFixedWidthElements = (innerWidth: number): [HTMLElement, number][] =>
-  getFixedElements()
-    .filter((elem) => {
-      const elementWidth = window
-        .getComputedStyle(elem, null)
-        .getPropertyValue("width");
-      return !elementWidth || elementWidth === `${innerWidth}px`;
-    })
-    .map((elem) => {
-      const elementWidth = window
-        .getComputedStyle(elem, null)
-        .getPropertyValue("width");
-      return [
-        elem,
-        elementWidth ? parseInt(elementWidth.replace("px", "")) : innerWidth,
-      ];
-    });
-
-const getFixedRightElements = (): [HTMLElement, number][] =>
-  getFixedElements()
-    .filter((elem) => {
-      const elementRight = window
-        .getComputedStyle(elem, null)
-        .getPropertyValue("right");
-      return elementRight === "0px";
-    })
-    .map((elem) => [elem, 0]);
 
 function App() {
   const dispatch = useDispatch();
 
   const events = useSelector(selectEvents);
   const [localEvents, setLocalEvents] = useState<ParsedEvent[]>([]);
-
-  const [fixedWidthElements, setFixedWidthElements] = useState<
-    [HTMLElement, number][] | null
-  >(null);
-  const [lastInnerWidth, setLastInnerWidth] = useState<number | null>(null);
 
   const saveEventCallback = (event: ParsedEvent) => {
     if ((event as UserEvent).target?.domPath) {
@@ -83,40 +38,6 @@ function App() {
       registerOnCloseCallback: () => {},
     });
   }, []);
-
-  const [innerWidth] = useWindowSize();
-  useEffect(() => {
-    if (!innerWidth) return;
-    const widthElementsToUpdate =
-      fixedWidthElements !== null
-        ? fixedWidthElements
-        : getFixedWidthElements(innerWidth);
-    const widthChange =
-      lastInnerWidth !== null ? innerWidth - lastInnerWidth : sideBarWith * -1;
-
-    // body and fixed width elements need to be resized everytime viewport changes
-    const updatedWidthElements = widthElementsToUpdate.map(
-      ([elem, oldWidth]) => {
-        const newWidth = oldWidth + widthChange;
-        elem.style.width = `${newWidth}px`;
-        // todo: what if elem no longer exists?
-        return [elem, newWidth] as [HTMLElement, number];
-      }
-    );
-    const body = document.getElementsByTagName("body")[0];
-    body.style.width = `${innerWidth - sideBarWith}px`;
-
-    // fixed right elements only need to be moved once
-    // todo: what about when new fixed right elements are added
-    if (!lastInnerWidth) {
-      getFixedRightElements().forEach(([elem, oldRight]) => {
-        elem.style.right = `${sideBarWith}px`;
-      });
-    }
-
-    setFixedWidthElements(updatedWidthElements);
-    setLastInnerWidth(innerWidth);
-  }, [innerWidth]);
 
   useNewToast();
 
@@ -172,10 +93,7 @@ function App() {
   }, [events]);
 
   return (
-    <div
-      className="fixed top-0 right-0 bottom-0 border-2 border-red-500 bg-white"
-      style={{ width: `${sideBarWith}px` }}
-    >
+    <Resizer>
       <div className="h-96 border">
         <ToastContainer
           containerId="my-toaster"
@@ -188,7 +106,7 @@ function App() {
         <ScrollBtn direction="up" />
         <ScrollBtn direction="down" />
       </div>
-    </div>
+    </Resizer>
   );
 }
 
---
+++ src/apps/widget/Resizer.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { useWindowSize } from "./hooks/useWindowSize";
+import { widgetId } from "../../widget";
+import { sideBarWith } from "./constants";
+
+const Resizer = ({ children }: { children: React.ReactNode }) => {
+  const [fixedWidthElements, setFixedWidthElements] = useState<
+    [HTMLElement, number][] | null
+  >(null);
+  const [lastInnerWidth, setLastInnerWidth] = useState<number | null>(null);
+  const [innerWidth] = useWindowSize();
+  useEffect(() => {
+    if (!innerWidth) return;
+    const widthElementsToUpdate =
+      fixedWidthElements !== null
+        ? fixedWidthElements
+        : getFixedWidthElements(innerWidth);
+    const widthChange =
+      lastInnerWidth !== null ? innerWidth - lastInnerWidth : sideBarWith * -1;
+
+    // body and fixed width elements need to be resized everytime viewport changes
+    const updatedWidthElements = widthElementsToUpdate.map(
+      ([elem, oldWidth]) => {
+        const newWidth = oldWidth + widthChange;
+        elem.style.width = `${newWidth}px`;
+        // todo: what if elem no longer exists?
+        return [elem, newWidth] as [HTMLElement, number];
+      }
+    );
+    const body = document.getElementsByTagName("body")[0];
+    body.style.width = `${innerWidth - sideBarWith}px`;
+
+    // fixed right elements only need to be moved once
+    // todo: what about when new fixed right elements are added
+    if (!lastInnerWidth) {
+      getFixedRightElements().forEach(([elem, oldRight]) => {
+        elem.style.right = `${sideBarWith}px`;
+      });
+    }
+
+    setFixedWidthElements(updatedWidthElements);
+    setLastInnerWidth(innerWidth);
+  }, [innerWidth]);
+  return (
+    <div
+      className="fixed top-0 right-0 bottom-0 border-2 border-red-500 bg-white"
+      style={{ width: `${sideBarWith}px` }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const getFixedElements = (): HTMLElement[] =>
+  Array.prototype.slice
+    .call(document.body.getElementsByTagName("*"))
+    .filter(
+      (elem) =>
+        window.getComputedStyle(elem, null).getPropertyValue("position") ==
+        "fixed"
+    )
+    .filter((elem) => !document.getElementById(widgetId)!.contains(elem));
+
+const getFixedWidthElements = (innerWidth: number): [HTMLElement, number][] =>
+  getFixedElements()
+    .filter((elem) => {
+      const elementWidth = window
+        .getComputedStyle(elem, null)
+        .getPropertyValue("width");
+      return !elementWidth || elementWidth === `${innerWidth}px`;
+    })
+    .map((elem) => {
+      const elementWidth = window
+        .getComputedStyle(elem, null)
+        .getPropertyValue("width");
+      return [
+        elem,
+        elementWidth ? parseInt(elementWidth.replace("px", "")) : innerWidth,
+      ];
+    });
+
+const getFixedRightElements = (): [HTMLElement, number][] =>
+  getFixedElements()
+    .filter((elem) => {
+      const elementRight = window
+        .getComputedStyle(elem, null)
+        .getPropertyValue("right");
+      return elementRight === "0px";
+    })
+    .map((elem) => [elem, 0]);
+
+export default Resizer;
