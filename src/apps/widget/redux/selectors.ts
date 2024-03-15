999051e413bf57f308103b02dae73036eaec3a2f
mstuartf <mstuartflood@gmail.com>
2024-03-15 08:23:36 +0000

    assertions

--- src/apps/widget/redux/selectors.ts
+++ src/apps/widget/redux/selectors.ts
@@ -4,6 +4,7 @@ export const selectEvents = (state: RootState) => state.root.events;
 export const selectRecordingInProgress = (state: RootState) =>
   state.root.recordingInProgress;
 export const selectHasRefreshed = (state: RootState) => state.root.hasRefreshed;
-export const selectBaseUrl = (state: RootState) => state.root.baseUrl;
+export const selectIsAddingAssertion = (state: RootState) =>
+  state.root.isAddingAssertion;
 export const selectDisplayEvents = (state: RootState) =>
   selectEvents(state).slice(-3);
