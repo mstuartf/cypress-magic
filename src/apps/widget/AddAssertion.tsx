2e2d747d8c90308e1c2d2212b41156a38d7e8975
Mike Flood <mike.flood@complyadvantage.com>
2024-03-16 07:36:28 +0000

    overlay at root level

--- src/apps/widget/AddAssertion.tsx
+++ src/apps/widget/AddAssertion.tsx
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsAddingAssertion } from "./redux/selectors";
 import { setIsAddingAssertion } from "./redux/slice";
 import { sideBarWith } from "./constants";
 
-export const assertionOverlayId = "__assertionOverlay__";
-
 const AddAssertion = () => {
   const dispatch = useDispatch();
   const isAddingAssertion = useSelector(selectIsAddingAssertion);
 
+  // needs to be added at the root level
+  useEffect(() => {
+    showOrHideOverlay(isAddingAssertion);
+  }, [isAddingAssertion]);
+
   return (
     <>
-      {isAddingAssertion && (
-        <div
-          id={assertionOverlayId}
-          className="fixed top-0 left-0 bottom-0 bg-red-500 opacity-15"
-          style={{ right: `${sideBarWith}px`, zIndex: 1000 }}
-        ></div>
-      )}
       <div>
         {isAddingAssertion ? (
           <div className="grid">
@@ -46,3 +42,18 @@ const AddAssertion = () => {
 };
 
 export default AddAssertion;
+
+export const assertionOverlayId = "__assertionOverlay__";
+
+const showOrHideOverlay = (show: boolean) => {
+  if (show) {
+    const el = window.document.createElement("div");
+    el.id = assertionOverlayId;
+    el.className = "fixed top-0 left-0 bottom-0 bg-red-500 opacity-15";
+    el.style.right = `${sideBarWith}px`;
+    el.style.zIndex = "1000";
+    window.document.body.appendChild(el);
+  } else {
+    document.getElementById(assertionOverlayId)?.remove();
+  }
+};
