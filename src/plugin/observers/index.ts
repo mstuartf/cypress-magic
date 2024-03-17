e8de4863e0bda3e260cd5baf7e73a345a1ca19b1
mstuartf <mstuartflood@gmail.com>
2024-03-17 21:26:12 +0000

    remove unused packages

--- src/plugin/observers/index.ts
+++ src/plugin/observers/index.ts
@@ -3,19 +3,13 @@ import { initFetchObserver } from "./fetch";
 import { initUserObserver } from "./user";
 import { initViewportObserver } from "./viewport";
 import { initXMLHttpRequestObserver } from "./xml-http-request";
-import { initCookieObserver } from "./cookies";
-import { initLocalStorageObserver } from "./localStorage";
-import { initSessionStorageObserver } from "./sessionStorage";
 
 export const initializers = {
   history: initHistoryObserver,
   fetch: initFetchObserver,
-  localStorage: initLocalStorageObserver,
-  sessionStorage: initSessionStorageObserver,
   user: initUserObserver,
   viewport: initViewportObserver,
   xml: initXMLHttpRequestObserver,
-  cookies: initCookieObserver,
 };
 
 export type Observer = keyof typeof initializers;
