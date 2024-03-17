bd90891d526bc4f29eefacfd1e1e808b9618e06d
mstuartf <mstuartflood@gmail.com>
2024-03-17 23:35:23 +0000

    use callback

--- src/apps/popup/components/Main.tsx
+++ src/apps/popup/components/Main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectTabId } from "../redux/selectors";
+import { selectHasBeenInjected, selectTabId } from "../redux/selectors";
 import { activateForTab, deactivateForTab } from "../redux/slice";
 import { getActiveTabId } from "../../../chrome/utils";
 
 const Main = () => {
   const tabId = useSelector(selectTabId);
+  const hasBeenInjected = useSelector(selectHasBeenInjected);
   const dispatch = useDispatch();
   const onClick = () => {
     if (!!tabId) {
@@ -16,7 +17,7 @@ const Main = () => {
   };
   return (
     <div>
-      main view {tabId}
+      main view {tabId} {`${hasBeenInjected}`}
       <div>
         <button onClick={onClick}>{!!tabId ? "deactivate" : "activate"}</button>
       </div>
--- src/apps/popup/redux/selectors.ts
+++ src/apps/popup/redux/selectors.ts
@@ -2,3 +2,5 @@ import { RootState } from "./store";
 
 export const selectCacheLoaded = (state: RootState) => state.root.cacheLoaded;
 export const selectTabId = (state: RootState) => state.root.tabId;
+export const selectHasBeenInjected = (state: RootState) =>
+  state.root.hasBeenInjected;
