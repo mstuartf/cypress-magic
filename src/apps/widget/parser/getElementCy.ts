18b8d111511c04e8301977c1cceaa5f6b718a778
mstuartf <mstuartflood@gmail.com>
2024-03-18 22:16:39 +0000

    update selector logic

--- src/apps/widget/parser/getElementCy.ts
+++ src/apps/widget/parser/getElementCy.ts
@@ -1,7 +1,6 @@
 import { Target } from "../../../plugin/types";
-import { formatAsJSLiteral } from "./formatAsJSLiteral";
 import { parseSelector } from "./parseSelector";
 
 export function getElementCy(domPath: Target["domPath"]): string {
-  return `cy.get(${formatAsJSLiteral(parseSelector(domPath))})`;
+  return `cy.get('${parseSelector(domPath)}')`;
 }
