bbec16f5a3124f44a3c6cf60253b496998ce4035
mstuartf <mstuartflood@gmail.com>
2024-03-18 00:30:04 +0000

    stop the content script being executed multiple times

--- src/widget.tsx
+++ src/widget.tsx
@@ -9,23 +9,32 @@ import initialize from "./plugin/initialize";
 import { ParsedEvent } from "./plugin/types";
 import { saveEvent } from "./apps/widget/redux/slice";
 
-// this needs to be done immediately (i.e. not in the app) to catch all events from the host page
-initialize({
-  saveEvent: (event: ParsedEvent) => store.dispatch(saveEvent(event)),
-  saveFixture: () => {},
-});
+interface W extends Window {
+  seasmokeHasLoaded?: boolean;
+}
 
-const createRootElement = () => {
-  const el = window.document.createElement("div");
-  el.id = widgetId;
-  window.document.body.appendChild(el);
-  return el;
-};
+const getHasLoaded = (): boolean =>
+  (window as any as W).seasmokeHasLoaded || false;
+const setHasLoaded = (value: boolean) =>
+  ((window as any as W).seasmokeHasLoaded = value);
 
 // content script runs in the main process and in the extension popup, so need this check here
 // because we only want to render the app in the main process
 const { protocol } = new URL(window.location.href);
-if (!protocol.includes("chrome-extension")) {
+if (!protocol.includes("chrome-extension") && !getHasLoaded()) {
+  // this needs to be done immediately (i.e. not in the app) to catch all events from the host page
+  initialize({
+    saveEvent: (event: ParsedEvent) => store.dispatch(saveEvent(event)),
+    saveFixture: () => {},
+  });
+
+  const createRootElement = () => {
+    const el = window.document.createElement("div");
+    el.id = widgetId;
+    window.document.body.appendChild(el);
+    return el;
+  };
+
   console.log("INJECTED");
   const root = ReactDOM.createRoot(
     window.document.getElementById(widgetId) || createRootElement()
@@ -37,4 +46,5 @@ if (!protocol.includes("chrome-extension")) {
       </Provider>
     </React.StrictMode>
   );
+  setHasLoaded(true);
 }
