15e8f624d0016486642a1ef92188bcc6c2a2eaa1
Mike Flood <mike.flood@complyadvantage.com>
2024-03-18 22:03:45 +0000

    scrolling content when more added

--- src/apps/widget/EventList.tsx
+++ src/apps/widget/EventList.tsx
@@ -1,11 +1,16 @@
 import { useSelector } from "react-redux";
 import { selectEvents } from "./redux/selectors";
 import Typewriter from "./Typewriter";
+import { useEffect, useRef, useState } from "react";
 
 const EventList = () => {
   const events = useSelector(selectEvents);
+  const ref = useScrollDownOnSizeIncrease();
   return (
-    <div className="cyw-flex-grow cyw-my-4 cyw-border cyw-border-gray-400 cyw-rounded cyw-p-2 cyw-overflow-scroll">
+    <div
+      ref={ref}
+      className="cyw-flex-grow cyw-my-4 cyw-border cyw-border-gray-400 cyw-rounded cyw-p-2 cyw-overflow-scroll"
+    >
       {events.map((event) => (
         <div
           key={event.timestamp}
@@ -21,3 +26,28 @@ const EventList = () => {
 };
 
 export default EventList;
+
+export const useScrollDownOnSizeIncrease = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  useEffect(() => {
+    if (ref.current) {
+      const observer = new MutationObserver(() => {
+        const newScrollHeight = ref.current?.scrollHeight || 0;
+        if (!!ref.current && newScrollHeight > scrollHeight) {
+          ref.current.scrollTop = ref.current.scrollHeight;
+        }
+        setScrollHeight(ref.current?.scrollHeight || 0);
+      });
+      observer.observe(ref.current, {
+        characterData: true,
+        childList: true,
+        subtree: true,
+        attributes: true,
+      });
+    }
+  }, [ref]);
+
+  return ref;
+};
