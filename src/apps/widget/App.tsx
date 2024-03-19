2e4f83ba34ccf22f582144dc11022a5017f5dead
Mike Flood <mike.flood@complyadvantage.com>
2024-03-19 11:49:21 +0000

    caching entire store

--- src/apps/widget/App.tsx
+++ src/apps/widget/App.tsx
@@ -1,13 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  setBaseUrl,
-  setHasRefreshed,
-  setRecordingInProgress,
-} from "./redux/slice";
+import { setHasRefreshed } from "./redux/slice";
 import Resizer from "./Resizer";
 import { selectRecordingInProgress } from "./redux/selectors";
-import { readCache } from "./cache";
 import RecordingInProgress from "./RecordingInProgress";
 import Setup from "./Setup";
 
@@ -16,17 +11,10 @@ function App() {
   const recordingInProgress = useSelector(selectRecordingInProgress);
 
   useEffect(() => {
-    const { recordingInProgress, baseUrl } = readCache();
-    dispatch(setRecordingInProgress(recordingInProgress));
-    dispatch(setBaseUrl(baseUrl));
     if (recordingInProgress) {
       dispatch(setHasRefreshed(true));
     }
   }, []);
-
-  if (recordingInProgress === undefined) {
-    return null;
-  }
 
   return (
     <Resizer>
