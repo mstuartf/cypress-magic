2ae7ebfe1d1959e634c857cfc45596665294601c
Mike Flood <mike.flood@complyadvantage.com>
2024-03-19 11:58:27 +0000

    remove viewport events

--- src/plugin/observers/index.ts
+++ src/plugin/observers/index.ts
@@ -1,14 +1,12 @@
 import { initHistoryObserver } from "./history";
 import { initFetchObserver } from "./fetch";
 import { initUserObserver } from "./user";
-import { initViewportObserver } from "./viewport";
 import { initXMLHttpRequestObserver } from "./xml-http-request";
 
 export const initializers = {
   history: initHistoryObserver,
   fetch: initFetchObserver,
   user: initUserObserver,
-  viewport: initViewportObserver,
   xml: initXMLHttpRequestObserver,
 };
 
