4d5d73abd3ec4bd416f595151255d08a8de857a5
mstuartf <mstuartflood@gmail.com>
2024-03-26 11:19:08 +0000

    correct resizing logic

--- src/apps/widget/hooks/useNewFixedElementAdded.ts
+++ src/apps/widget/hooks/useNewFixedElementAdded.ts
@@ -3,28 +3,77 @@ import { sideBarWith, widgetId } from "../constants";
 
 export const useNewFixedElementAdded = () => {
   useMutateObserver(document.body, (mutations, observer) => {
-    getFixedRightHTMLElements([
+    const nodes = [
       ...mutations.flatMap(
         ({ addedNodes }) => Array.prototype.slice.call(addedNodes) as Node[]
       ),
       ...mutations.map(({ target }) => target),
-    ]).forEach(([elem, right]) => {
+    ];
+    const elements: Element[] = nodes.filter((node): node is Element =>
+      isElement(node)
+    );
+    filterFixedSetRightHTMLElements(elements).forEach(([elem, right]) => {
       elem.style.setProperty("right", `${sideBarWith + right}px`, "important");
     });
   });
 };
 
-const getFixedRightHTMLElements = (nodes: Node[]): [HTMLElement, number][] => {
-  return nodes
-    .filter((node): node is Element => isElement(node))
+export const getFixedFullWidthElements = (
+  innerWidth: number
+): [HTMLElement, number][] =>
+  Array.prototype.slice
+    .call(document.body.getElementsByTagName("*"))
+    .filter((elem) => isNotInWidget(elem))
+    .filter((elem) => isPositionFixed(elem))
+    .filter((elem) => {
+      const elementWidth = window
+        .getComputedStyle(elem, null)
+        .getPropertyValue("width");
+      return !elementWidth || elementWidth === `${innerWidth}px`;
+    })
+    .map((elem) => {
+      const elementWidth = window
+        .getComputedStyle(elem, null)
+        .getPropertyValue("width");
+      return [
+        elem,
+        elementWidth ? parseInt(elementWidth.replace("px", "")) : innerWidth,
+      ];
+    });
+
+export const getFixedSetRightElements = (): [HTMLElement, number][] =>
+  filterFixedSetRightHTMLElements(
+    Array.prototype.slice.call(document.body.getElementsByTagName("*"))
+  );
+
+export const filterFixedSetRightHTMLElements = (
+  elements: Element[]
+): [HTMLElement, number][] => {
+  return elements
     .filter((element): element is HTMLElement => isHTMLElement(element))
-    .filter((element) => isHTMLElement(element))
-    .filter((elem) => !document.getElementById(widgetId)!.contains(elem))
-    .map((element) => isFixedRight(element))
+    .filter((elem) => isNotInWidget(elem))
+    .filter((elem) => isPositionFixed(elem))
+    .filter((elem) => isSetRight(elem))
+    .map((element) => getFixedRight(element))
     .filter(([element, right]) => right < sideBarWith);
 };
 
-export const isFixedRight = (element: HTMLElement): [HTMLElement, number] => {
+export const isPositionFixed = (element: HTMLElement): boolean => {
+  return (
+    window.getComputedStyle(element, null).getPropertyValue("position") ===
+    "fixed"
+  );
+};
+
+export const isSetRight = (element: HTMLElement): boolean => {
+  return element.style.right !== "";
+};
+
+export const isNotInWidget = (element: HTMLElement): boolean => {
+  return !document.getElementById(widgetId)!.contains(element);
+};
+
+export const getFixedRight = (element: HTMLElement): [HTMLElement, number] => {
   const elementRight = parseInt(
     window
       .getComputedStyle(element, null)
