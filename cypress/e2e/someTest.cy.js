14bdbd674d3e25150fc37b3e5702cea99d73011f
mstuartf <mstuartflood@gmail.com>
2024-03-27 07:42:18 +0000

    re-add cypress

---
+++ cypress/e2e/someTest.cy.js
@@ -0,0 +1,13 @@
+describe("SomeTest", () => {
+  it("should work", () => {
+    cy.visit("./demo/index.html");
+    cy.intercept("GET", "https://swapi.dev/api/people/1", {
+      statusCode: 200,
+      fixture: "someTest/GET___api_people_1_.json",
+    }).as("GET__/api/people/1/");
+    cy.get("#request-button").click();
+    cy.wait("@GET__/api/people/1/");
+    // cy.get('#change-selector-button').click();
+    cy.get('[data-cy="select-this-element"]').click();
+  });
+});
