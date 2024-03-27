c0999ccbcf29c59081767e58514eeca8f96618ac
Mike Flood <mike.flood@complyadvantage.com>
2024-03-27 10:20:47 +0000

    move toggle

--- src/apps/widget/components/Toggle.tsx
+++ src/apps/widget/components/Toggle.tsx
@@ -18,8 +18,8 @@ const Toggle = ({
         checked={checked}
         onChange={onChange}
       />
-      <div className="cyw-relative cyw-w-9 cyw-h-5 cyw-bg-slate-300 peer-focus:cyw-outline-none peer-focus:cyw-ring-2 peer-focus:cyw-ring-blue-300 cyw-rounded-full cyw-peer peer-checked:after:cyw-translate-x-full rtl:peer-checked:after:-cyw-translate-x-full peer-checked:after:cyw-border-white after:cyw-content-[''] after:cyw-absolute after:cyw-top-[2px] after:cyw-start-[2px] after:cyw-bg-white after:cyw-border-gray-300 after:cyw-border after:cyw-rounded-full after:cyw-h-4 after:cyw-w-4 after:cyw-transition-all peer-checked:cyw-bg-emerald-500"></div>
-      <span className="cyw-ms-3 cyw-text-xs cyw-text-white">{label}</span>
+      <div className="cyw-relative cyw-w-7 cyw-h-4 cyw-bg-slate-300 peer-focus:cyw-outline-none peer-focus:cyw-ring-2 peer-focus:cyw-ring-blue-300 cyw-rounded-full cyw-peer peer-checked:after:cyw-translate-x-full rtl:peer-checked:after:-cyw-translate-x-full peer-checked:after:cyw-border-white after:cyw-content-[''] after:cyw-absolute after:cyw-top-[2px] after:cyw-start-[2px] after:cyw-bg-white after:cyw-border-gray-300 after:cyw-border after:cyw-rounded-full after:cyw-h-3 after:cyw-w-3 after:cyw-transition-all peer-checked:cyw-bg-emerald-500"></div>
+      <span className="cyw-ms-3 cyw-text-xs">{label}</span>
     </label>
   </div>
 );
