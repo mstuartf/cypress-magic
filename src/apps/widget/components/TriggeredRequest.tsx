9a8a093f49b8be503538894c49d92b40b4f67801
mstuartf <mstuartflood@gmail.com>
2024-04-05 08:54:40 +0100

    filter xml by url

--- src/apps/widget/components/TriggeredRequest.tsx
+++ src/apps/widget/components/TriggeredRequest.tsx
@@ -6,7 +6,7 @@ const TriggeredRequest = ({ event }: { event: RequestEvent }) => (
   <div className="cyw-ml-14 cyw-mb-2 cyw-italic cyw-font-semibold">
     <div className="cyw-flex cyw-justify-between cyw-items-center">
       <div className="cyw-flex cyw-items-center">
-        <div className="cyw-break-keep">(fetch)</div>
+        <div className="cyw-break-keep">({event.initiator})</div>
         <div className="cyw-mx-4 cyw-break-keep">
           {event.method.toUpperCase()}&nbsp;&nbsp;{event.status}
         </div>
