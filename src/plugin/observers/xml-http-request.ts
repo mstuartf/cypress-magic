d654095ffa7bca69b4c286c47371d765eb4df11e
Mike Flood <mike.flood@complyadvantage.com>
2024-04-05 10:29:52 +0100

    xml support mocking

--- src/plugin/observers/xml-http-request.ts
+++ src/plugin/observers/xml-http-request.ts
@@ -3,11 +3,15 @@ import {
   aliasToFileName,
   getBlobFileExtension,
   pickleBlob,
+  PickledBlob,
+  unPickleBlob,
 } from "../utils/pickleBlob";
 import { getAbsoluteUrl } from "../utils/absoluteUrls";
 import { generateEventId } from "../utils/generateEventId";
+import { buildRequestAlias } from "../utils/aliases";
 
 // should only record events to matching URLs
+// should not make network calls when running test in mock mode
 
 interface PatchedXMLHttpRequest extends XMLHttpRequest {
   __meta: {
@@ -16,6 +20,12 @@ interface PatchedXMLHttpRequest extends XMLHttpRequest {
     id: string;
     alias: string;
   };
+  // make these writeable for mocking
+  readyState: number;
+  status: number;
+  statusText: string;
+  responseText: string;
+  response: any;
 }
 
 export function initXMLHttpRequestObserver({
@@ -28,42 +38,82 @@ export function initXMLHttpRequestObserver({
 }: InitArgs) {
   const _open = window.XMLHttpRequest.prototype.open;
   window.XMLHttpRequest.prototype.open = function () {
+    const instance = this as PatchedXMLHttpRequest;
     const url = getAbsoluteUrl(arguments[1]);
 
-    if (matchUrl(url)) {
-      const instance = this as PatchedXMLHttpRequest;
-      // cache these so they are available in onreadystatechange
-      instance.__meta = {
-        method: arguments[0].toUpperCase(),
-        id: generateEventId(),
-        url,
-        alias: buildAlias({
-          url,
-          method: arguments[0].toUpperCase(),
-        }),
-      };
-      saveEvent({
-        type: "request",
-        timestamp: Date.now(),
-        id: instance.__meta.id,
-        url: instance.__meta.url,
-        method: instance.__meta.method,
-        alias: instance.__meta.alias,
-        initiator: "xml",
-      });
+    if (!matchUrl(url)) {
+      return _open.apply(this, arguments as any);
     }
+
+    // cache these so they are available in onreadystatechange
+    instance.__meta = {
+      method: arguments[0].toUpperCase(),
+      id: generateEventId(),
+      url,
+      alias: buildAlias({
+        url,
+        method: arguments[0].toUpperCase(),
+      }),
+    };
+
+    if (mockApiCalls()) {
+      // do not trigger the actual network request
+      return;
+    }
+
+    saveEvent({
+      type: "request",
+      timestamp: Date.now(),
+      id: instance.__meta.id,
+      url: instance.__meta.url,
+      method: instance.__meta.method,
+      alias: instance.__meta.alias,
+      initiator: "xml",
+    });
 
     return _open.apply(this, arguments as any);
   };
 
   const _send = window.XMLHttpRequest.prototype.send;
-  window.XMLHttpRequest.prototype.send = function () {
+  window.XMLHttpRequest.prototype.send = function (...sendArgs) {
     const instance = this as PatchedXMLHttpRequest;
     const _onreadystatechange = this.onreadystatechange;
-    this.onreadystatechange = function () {
-      const url = instance.__meta.url;
+    const _onload = this.onload;
 
-      if (this.readyState === 4 && matchUrl(url)) {
+    const url = instance.__meta.url;
+    if (!matchUrl(url)) {
+      return _send.apply(this, sendArgs);
+    }
+
+    if (mockApiCalls()) {
+      const mockedResponse = getMockedResponse(instance.__meta.alias);
+
+      if (!mockedResponse) {
+        throw Error(
+          `could not find mocked response for ${instance.__meta.alias}`
+        );
+      }
+
+      const { status, statusText, content } = mockedResponse;
+
+      buildMockedResponse(content).then((jsonString) => {
+        forceUpdateResponse(instance, status, statusText, jsonString);
+
+        if (_onreadystatechange) {
+          // todo event?
+          _onreadystatechange.apply(this, [{} as Event]);
+        }
+
+        if (_onload) {
+          _onload.apply(this, [{} as ProgressEvent]);
+        }
+      });
+
+      return;
+    }
+
+    this.onreadystatechange = function (...onReadyStateChangeArgs) {
+      if (this.readyState === 4) {
         const { method, id: requestId, alias } = instance.__meta;
         const status = this.status;
         const event: Omit<ResponseEvent, "body" | "fixture"> = {
@@ -79,6 +129,7 @@ export function initXMLHttpRequestObserver({
         };
         try {
           if (status === 204) {
+            // todo
             saveEvent({ ...event, fixture: null });
             return;
           }
@@ -102,10 +153,49 @@ export function initXMLHttpRequestObserver({
         }
       }
 
+      // if the app under test has defined this (which they likely would have)
       if (_onreadystatechange) {
-        return _onreadystatechange.apply(this, arguments as any);
+        return _onreadystatechange.apply(this, onReadyStateChangeArgs);
       }
     };
-    return _send.apply(this, arguments as any);
+
+    return _send.apply(this, sendArgs);
   };
 }
+
+// so far only supports json responses
+async function buildMockedResponse(content: PickledBlob): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    unPickleBlob(content)
+      .then((blob) => blob.text())
+      .then((jsonString) => resolve(jsonString))
+      .catch((error) => reject(error));
+  });
+}
+
+const forceWriteableProps = [
+  "response",
+  "responseText",
+  "readyState",
+  "status",
+  "statusText",
+];
+const forceUpdateResponse = (
+  instance: PatchedXMLHttpRequest,
+  status: number,
+  statusText: string,
+  responseText: string
+) => {
+  forceWriteableProps.forEach((p) =>
+    Object.defineProperty(instance, p, { writable: true })
+  );
+  instance.readyState = 4;
+  instance.response = JSON.parse(responseText);
+  instance.responseText =
+    instance.responseType !== "json" ? responseText : instance.response;
+  instance.status = status;
+  instance.statusText = statusText;
+  forceWriteableProps.forEach((p) =>
+    Object.defineProperty(instance, p, { writable: false })
+  );
+};
