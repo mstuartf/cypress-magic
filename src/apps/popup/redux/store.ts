0fbb8464544328f57324de9f1f5a8e3dcd76f0d2
mstuartf <mstuartflood@gmail.com>
2024-04-09 12:10:03 +0100

    refresh user info

--- src/apps/popup/redux/store.ts
+++ src/apps/popup/redux/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { alias, wrapStore } from "webext-redux";
 import { rootReducer } from "./reducers";
 import { inject } from "../../../chrome/utils";
-import { activateForTab } from "./slice";
+import { activateForTab, refreshUserInfo, saveUserInfo } from "./slice";
 
 export type PopupState = ReturnType<typeof rootReducer>;
 
@@ -10,6 +10,12 @@ export type PopupState = ReturnType<typeof rootReducer>;
 export const middlewareAliases: { [key: string]: (action: any) => any } = {
   [activateForTab.type]: (action: ReturnType<typeof activateForTab>) => {
     inject(action.payload);
+    return action;
+  },
+  [refreshUserInfo.type]: (action: ReturnType<typeof refreshUserInfo>) => {
+    chrome.identity.getProfileUserInfo({}, (res) =>
+      store.dispatch(saveUserInfo(res))
+    );
     return action;
   },
 };
--- src/apps/widget/components/RunTest.tsx
+++ src/apps/widget/components/RunTest.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { setIsRunning } from "../redux/slice";
 import { Tooltip } from "react-tooltip";
+import Refresh from "../../shared/Refresh";
 
 const RunTest = () => {
   const dispatch = useDispatch();
@@ -12,22 +13,7 @@ const RunTest = () => {
         data-tooltip-content="Run test"
         onClick={() => dispatch(setIsRunning(true))}
       >
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M13 8C13 10.7614 10.7614 13 8 13C5.23858 13 3 10.7614 3 8C3 5.23858 5.23858 3 8 3H11M11 3L9 5M11 3L9 1"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="icon-light"
-          ></path>
-        </svg>
+        <Refresh />
       </button>
       <Tooltip id="run-test-tooltip" className="cyw-text-xs" />
     </>
